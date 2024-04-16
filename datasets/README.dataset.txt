7fb55f67c9e8ab91d224bdf976c92a046c540bcc
Noor30903 <nolalwlaqi@effat.edu.sa>
2024-04-16 16:41:08 +0300

    adding the dataset
    both box dataset is added to the data.yaml
    
    Co-Authored-By: Maha182 <148116369+Maha182@users.noreply.github.com>
    Co-Authored-By: mashaelmoh <151399359+mashaelmoh@users.noreply.github.com>

--- datasets/README.dataset.txt
+++
@@ -1,6 +0,0 @@
-# box detection  > 2023-07-30 2:54pm
-https://universe.roboflow.com/ai-project-yolo/box-detection-rdssl
-
-Provided by a Roboflow user
-License: CC BY 4.0
-
--- train.py
+++ train.py
@@ -2,6 +2,9 @@ from ultralytics import YOLO
 from pathlib import Path
 import argparse
 import settings
+import torch
+
+
 def train():
     """
     Function to train the YOLOv8 model.
@@ -9,9 +12,15 @@ def train():
     # Initialize model for training
     model_path = Path(settings.DETECTION_MODEL)
     model = YOLO(model_path)
-    # Start training
-    model.train(data='datasets/data.yaml', epochs=100, imgsz=640)
-    model.save(model_path)
+    
+    #freeze_until = "model.model.6" 
+    #for name, parameter in model.named_parameters():
+    #    if name.startswith(freeze_until):
+    #        break
+    #    parameter.requires_grad = False
+
+    model.train(data='datasets/data.yaml', epochs=100, imgsz=640, resume=True)
+    
 
 if __name__ == "__main__":
     train()
